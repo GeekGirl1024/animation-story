e54a3e02c5e18f11a097a69973f71f906b7e2d3a
Sophia Lee <sophia@transgirltheory.com>
2021-10-18 12:12:42 -0700

    initial node.js app

---
+++ app.js
@@ -0,0 +1,8 @@
+// Created on Sophia’s iPad.
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send("Hello World"))
+
+app.listen(port, () => console.log(`Application listening on port ${port}!`))
