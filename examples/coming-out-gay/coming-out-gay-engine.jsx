8eec7eb4dc37ec05fe2c0788ea86b781175e4e32
Sophia Lee <sophia@transgirltheory.com>
2022-02-19 12:30:19 -0800

    Update boy girl system to be determined via centeral point

--- examples/coming-out-gay/coming-out-gay-engine.jsx
+++ examples/coming-out-gay/coming-out-gay-engine.jsx
@@ -11,28 +11,34 @@ const startingConditions = {
     girlColor: "#FFBBBB",
     pairs:[
       {
-        boy: { x: 10, y: 10 },
-        girl: { x: 20, y: 20}
+        center: { x: 0, y: 0},
+        angle: 2 * Math.PI / 4,
+        boy: {},
+        girl: {}
       },
       {
-        boy: { x: 40, y: 0 },
-        girl: { x: 50, y: 10}
+        center: { x: 40, y: 0},
+        angle: 2 * Math.PI / 3,
+        boy: {},
+        girl: {}
       },
       {
-        boy: { x: 0, y: 40 },
-        girl: { x: 10, y: 50}
+        center: { x: 75, y: 30},
+        angle: 2 * Math.PI / 2,
+        boy: {},
+        girl: {}
       },
       {
-        boy: { x: 75, y: 30 },
-        girl: { x: 85, y: 40}
+        center: { x: -40, y: -10},
+        angle: 2 * Math.PI * 5./6,
+        boy: {},
+        girl: {}
       },
       {
-        boy: { x: -40, y: 0 },
-        girl: { x: -30 , y: 10}
-      },
-      {
-        boy: { x: -80, y: 30 },
-        girl: { x: -70 , y: 40}
+        center: { x: -80, y: 30},
+        angle: 2 * Math.PI * 3./5,
+        boy: {},
+        girl: {}
       }
     ]
   };
@@ -48,35 +54,48 @@ class ComingOutGayEngine extends Engine {
     
     this.boyGirlPairs = [];
     
-    let dotCircleDeltaRadians = (2*Math.PI) / 3000.;
+    let dotCircleDeltaRadians = (2*Math.PI) / 1500.;
 
     for(let i = 0; i < startingConditions.pairs.length; i++){
       let pair = startingConditions.pairs[i];
-      let boy = new Dot(pair.boy.x, pair.boy.y, 4, startingConditions.boyColor, "#000000");
-      let girl = new Dot(pair.girl.x, pair.girl.y, 4, startingConditions.girlColor, "#000000");
+      
+      let boy = new Dot(
+        pair.center.x + 10 * Math.cos(pair.angle),
+        pair.center.y + 10 * Math.sin(pair.angle),
+        4,
+        startingConditions.boyColor,
+        "#000000");
+
+      let girl = new Dot(
+        pair.center.x + 10 * Math.cos(pair.angle + Math.PI),
+        pair.center.y + 10 * Math.sin(pair.angle + Math.PI),
+        4,
+        startingConditions.girlColor,
+        "#000000");
+
       let dotPair = { boy:boy, girl:girl}
       this.boyGirlPairs.push(dotPair);
       this.animationObjects.push(boy);
       this.animationObjects.push(girl);
 
-      boy.AddUpdateFunction(new MovementObject(0, 10000, null, null, 
+      boy.AddUpdateFunction(new MovementObject(0, 100000, null, null, 
         AnimationMovements.Spiral.bind(boy),
           {
             deltaRadians: dotCircleDeltaRadians,
             deltaRadius: 0,
-            centerX: boy.position.x + 5,
-            centerY:boy.position.y + 5
+            centerX: pair.center.x,
+            centerY: pair.center.y
           }
         )
       );
 
-      girl.AddUpdateFunction(new MovementObject(0, 10000, null, null, 
+      girl.AddUpdateFunction(new MovementObject(0, 100000, null, null, 
           AnimationMovements.Spiral.bind(girl),
           {
             deltaRadians: dotCircleDeltaRadians,
             deltaRadius: 0,
-            centerX: boy.position.x + 5,
-            centerY:boy.position.y + 5
+            centerX: pair.center.x,
+            centerY: pair.center.y
           }
         )
       );
